05ea52ea12d9d58792829db09b8f5e96de449114
ayushgptaa <ayush.gupta.jk@gmail.com>
2022-10-01 13:42:58 +0530

    📦 NEW:  add component for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,36 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: ["plugin:react/recommended", "airbnb", "prettier"],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: "latest",
-		sourceType: "module",
-	},
-	plugins: ["react"],
-	settings: {
-		react: {
-			version: "detect",
-		},
-	},
-	rules: {
-		"no-console": "warn",
-		"react/prop-types": "warn",
-		"react/jsx-filename-extension": 0,
-		"react/jsx-props-no-spreading": 0,
-		"react/react-in-jsx-scope": 0,
-		"import/no-unresolved": 0,
-	},
-};
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["plugin:react/recommended", "airbnb", "prettier"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    rules: {
+        "no-console": "warn",
+        "react/prop-types": "warn",
+        "react/jsx-filename-extension": 0,
+        "react/jsx-props-no-spreading": 0,
+        "react/react-in-jsx-scope": 0,
+        "import/no-unresolved": 0,
+        "arrow-body-style": 0,
+        "react/function-component-definition": [
+            1,
+            {
+                namedComponents: "arrow-function",
+                unnamedComponents: "arrow-function",
+            },
+        ],
+    },
+}
