8e37f7534242e94f741bd1eea9e476ecae936101
ayushgptaa <ayush.gupta.jk@gmail.com>
2023-04-14 16:27:22 +0530

    🐛 FIX: PausePlay Button Icon

--- src/components/Icons/PausePlayBtn.js
+++ src/components/Icons/PausePlayBtn.js
@@ -1,7 +1,7 @@
 import IconContainer from "../IconContainer"
 
 export const PausepauseBtn = ({pause}) => {
-    const src = pause ? "/images/Icons/playIcon.svg" : "/images/Icons/pauseIcon.svg"
+    const src = pause ? "/images/icons/playIcon.svg" : "/images/icons/pauseIcon.svg"
     const alt = pause ? "Play Button" : "Pause Button"
 
     return <IconContainer src={src} alt={alt} height={26} width={26} />
